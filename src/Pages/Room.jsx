7055dadc56b426b8f69a7588514ece9a4004fb7a
Abu-Sayed-Sarker <abusayed96728@gmail.com>
2025-09-01 10:40:55 +0600

    new

--- src/Pages/Room.jsx
+++ src/Pages/Room.jsx
@@ -47,8 +47,8 @@ export default function App() {
     const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
       appID,
       serverSecret,
-      null,
       roomID,
+      userName,
       userName
     );
 
